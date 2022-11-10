83bb1a991b322d480d7c6699cf9426d9429325c3
Wilfried Loche <wloche@hotmail.com>
2022-11-10 16:49:57 -0600

    Resolves #24: Provide a list of parents on Jira-ids

--- bulk-sub_task.js
+++ bulk-sub_task.js
@@ -7,13 +7,14 @@
  */
 
 /* ###### Parameters to be updated ###### */
-const JIRA_ID_PARENT = "DREG-642";
+const JIRA_ID_PARENTS = ["DREG-642", "DREG-643", "DREG-644", "DREG-645", "DREG-646", "DREG-648"];
 const users = [
-    { "name" : "Jorge", "id": "5f04b6341a26ad0014f24136" },
-    { "name" : "Artem", "id": "61bb53004fc7af00716b6e2f" },
-    { "name" : "Kate", "id": "60b1474dc5196d007009e5ee" },
-    { "name" : "Valera", "id": "6148f26578b7a1006aa9660f" },
+    // { "name" : "Jorge", "id": "5f04b6341a26ad0014f24136" },
+    // { "name" : "Artem", "id": "61bb53004fc7af00716b6e2f" },
+    // { "name" : "Kate", "id": "60b1474dc5196d007009e5ee" },
+    // { "name" : "Valera", "id": "6148f26578b7a1006aa9660f" },
     // { "name" : "Shou", "id": "605bce1bb18de5007101d957" },
+    { "name" : "Przemyslaw", "id": "62722676d7fd480068d86537" },
 ];
 
 /* ###### End of parameters section ###### */
